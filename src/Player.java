ffc95dc612818694348deab483e9f0da21a8d06c
Julien Bertozzi <bertozzijulien@gmail.com>
2014-11-18 23:07:42 +0100

    39.45

--- src/Player.java
+++ src/Player.java
@@ -24,8 +24,8 @@ class Player {
 
         while (true) {
             int platinum = in.nextInt(); // my available Platinum
-//            if (firstTurn && playerCount > 2)
-//                platinum *= 0.5f;
+            if (firstTurn && playerCount > 2)
+                platinum *= 0.5f;
             in.nextLine();
             long begin = System.currentTimeMillis();
             /***
